febaec7e40c3183e6e1f72d8bd411f6ed407b26c
WTFender <wtfender.cs@gmail.com>
2020-09-27 22:54:03 -0700

    add basic funcs

--- plugin/defang.js
+++ plugin/defang.js
@@ -1,67 +1,67 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-/* global global, Office, self, window */
-Office.onReady(() => {
-    // If needed, Office.js is ready to be called
-  });
-  
-  /**
-   * Shows a notification when the add-in command is executed.
-   * @param event {Office.AddinCommands.Event}
-   */
-  function action(event) {
-    const message = {
-      type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-      message: "Defang Test.",
-      icon: "Icon.80x80",
-      persistent: true
-    };
-  
-    // Show a notification message
-    Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-  
-    console.log("defang test")
-  
-    // Be sure to indicate when the add-in command function is complete
-    event.completed();
+Office.onReady(() => {});
+
+function detectPatterns(text){
+  matches = []
+  // TODO define patterns
+  patterns = [
+    /8\.8\.8\.8/igm,
+    /\.com/igm,
+    /[^\s\\]+:\/\//igm // TODO discard matches over 2,083 chars
+  ]
+  for (p in patterns){
+    match = text.match(patterns[p])
+    if (match){
+      matches = matches.concat(match)
+    }
   }
+  return matches
+}
 
-  /*
-  function testFunc(event) {
-    const message = {
-      type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-      message: "Defang testFunc.",
-      icon: "Icon.80x80",
-      persistent: true
-    };
-  
-    // Show a notification message
-    Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-  
-    console.log("defang testFunc")
-
-  
-    // Be sure to indicate when the add-in command function is complete
-    event.completed();
+function sanitizeMessage(messageBody){
+  bodyText = messageBody['value']
+  matches = detectPatterns(bodyText)
+  if (matches.length > 0){ 
+    console.log('matches')
+    console.log(matches)
+    // TODO replace matches
+    // replaceMatches(matches, text)
   }
-  */
-  
-  function getGlobal() {
-    return typeof self !== "undefined"
-      ? self
-      : typeof window !== "undefined"
-      ? window
-      : typeof global !== "undefined"
-      ? global
-      : undefined;
-  }
-  
-  const g = getGlobal();
+  return matches
+}
 
+function defang(event) {
+  // get current message
+  Office.context.mailbox.item.body.getAsync("text", {asyncContext: "msgBody"},
+    function(messageBody){
 
-  // the add-in command functions need to be available in global scope
-  g.action = action;
-  //g.testFunc = testFunc;
+      // sanitize
+      matches = sanitizeMessage(messageBody)
+
+      // notify
+      msgText = matches.length + ' items defanged.'
+      const message = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+        message: msgText,
+        icon: "Icon.80x80",
+        persistent: true
+      };
+      Office.context.mailbox.item.notificationMessages.replaceAsync("defang", message);
+    }
+  );
+  event.completed();
+}
+  
+function getGlobal() {
+  return typeof self !== "undefined"
+    ? self
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : undefined;
+}
+
+// register funcs for add-in
+const g = getGlobal();
+g.defang = defang;
   

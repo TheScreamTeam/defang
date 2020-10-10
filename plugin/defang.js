b49709e3e84e45e8d35cb440f1d00fca1b5134be
WTFender <wtfender.cs@gmail.com>
2020-10-09 22:24:21 -0700

    update preview funcs

--- plugin/defang.js
+++ plugin/defang.js
@@ -15,36 +15,63 @@ function detectPatterns(bodyText){
 }
 
 function sanitizeMessage(message){
+  var sanitizedBody = message
   var matches = detectPatterns(message)
+  console.log('var matches')
+  console.log(matches)
   if (matches.length > 0){ 
     for (var m in matches){
-      console.log(matches[m])
+      var sanitized = ''
+      if (matches[m]['pattern'] == 'ip' || matches[m]['pattern'] == 'domain'){
+        console.log('sanitized var')
+        sanitized = matches[m]['match'].replaceAll('.', '[.]')
+        console.log(sanitized)
+      }
+      console.log('var message')
+      console.log(message)
+
+      sanitizedBody = sanitizedBody.replaceAll(matches[m]['match'], sanitized)
+      console.log('sanitizedBody var')
+      console.log(sanitizedBody)
     }
-    // TODO replace matches
-    // replaceMatches(matches, text)
   }
-  return matches
-}
-
-function defang(event) {
-  // get current message
-  Office.context.mailbox.item.body.getAsync("html", {asyncContext: "msgBody"},
-    function(messageBody){
-      // sanitize
-      var matches = sanitizeMessage(messageBody['value'])
-
-      // notify
-      var msgText = matches.length + ' items defanged.'
-      const message = {
-        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-        message: msgText,
-        icon: "Icon.80x80",
-        persistent: true
-      };
-      Office.context.mailbox.item.notificationMessages.replaceAsync("defang", message);
+  Office.context.mailbox.item.body.setAsync('', {asyncContext: "msgBody"},
+    function(message){
+      Office.context.mailbox.item.body.setSelectedDataAsync(
+        sanitizedBody,
+        { coercionType: Office.CoercionType.Html, 
+        asyncContext: { var3: 1, var4: 2 } },
+        function (asyncResult) {
+          console.log('update body')
+        }
+        )
     }
   );
-  try {event.completed()} catch(e) {if (e instanceof TypeError){}}
+
+}
+
+function sanitize(event) {
+  Office.context.mailbox.item.body.getAsync("html", {asyncContext: "msgBody"},
+  function(message){
+    sanitizeMessage(message['value'])
+  })
+
+  Office.context.mailbox.item.body.getAsync("html", {asyncContext: "msgBody"},
+  function(message){
+    sanitizeMessage(message['value'])
+    console.log('es')
+    /*
+    var msgText = 'Message sanitized.'
+    const notify = {
+      type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+      message: msgText,
+      icon: "Icon.80x80",
+      persistent: true
+    };
+    Office.context.mailbox.item.notificationMessages.replaceAsync("sanitize", notify);
+    */
+  })
+  event.completed()
 }
   
 function getGlobal() {
@@ -57,10 +84,6 @@ function getGlobal() {
     : undefined;
 }
 
-// register funcs for add-in
-const g = getGlobal();
-g.defang = defang;
-
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -68,9 +91,9 @@ Office.onReady(info => {
     document.getElementById("detect").onclick = function(){
       Office.context.mailbox.item.body.getAsync("html", {asyncContext: "msgBody"},
       function(message){
+        document.getElementById("results").innerHTML = "";
         var detects = detectPatterns(message['value'])
         for (var d in detects){
-
           var value = detects[d]['pattern'] + ': ' + detects[d]['match']
           var textnode = document.createTextNode(value);
           var node = document.createElement("li")
@@ -81,5 +104,16 @@ Office.onReady(info => {
       }
     );
     };
+    document.getElementById("sanitize").onclick = function(){
+      Office.context.mailbox.item.body.getAsync("html", {asyncContext: "msgBody"},
+      function(message){
+        sanitizeMessage(message['value'])
+      })
+    }
   }
 });
+
+
+// register funcs for add-in
+const g = getGlobal();
+g.sanitize = sanitize;
